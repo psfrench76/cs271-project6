2fa2b2c1c57efb0582b2fb6c848d8dca0762b155
Patrick French <psfrench76@gmail.com>
2023-12-05 18:57:15 -0800

    Initial commit

---
+++ Proj6_frenchp.asm
@@ -0,0 +1,27 @@
+TITLE String Converter   (Proj6_frenchp.asm)
+
+; Author: Patrick French
+; Last Modified: 12-5-23
+; OSU email address: frenchp@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number:  6            Due Date: 12-10-23
+; Description: TODO - description
+;
+; TODO
+;
+
+INCLUDE Irvine32.inc
+
+
+.data
+
+
+.code
+main PROC
+
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+
+END main
